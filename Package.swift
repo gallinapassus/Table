b87bb569470ce6e7f5272543e70e14f16612e5d8
gallinapassus <gallinapassus@local>
2023-09-18 17:43:35 +0300

    Updated to Swift tools 5.5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         //.package(path: "../Combinations"),
         .package(url: "https://github.com/gallinapassus/DebugKit.git",
-                 from: "0.0.4"),
+                 branch: "main"),
     ],
     targets: [
         .target(
